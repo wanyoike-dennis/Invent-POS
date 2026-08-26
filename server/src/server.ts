3b196b6416f6682dfedaad4c4de7df59fd102011
Wanyoike-dennis <dennixtrevor@gmail.com>
2026-08-26 14:41:19 +0300

    feat(auth): add token authentication middleware to product and category routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import { authenticateToken } from "./middleware/authMiddleware.js";
 
 dotenv.config();
 
@@ -19,8 +20,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/categories", categoryRoutes);
+app.use("/api/products", authenticateToken, productRoutes);
+app.use("/api/categories", authenticateToken, categoryRoutes);
 
 
 const PORT = process.env.PORT || 5000;
